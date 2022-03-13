b933e5c81642c944a5a9fb2941a7e69c0c2bec05
AlisaZhuk <Ahata01102011@gmail.com>
2022-03-13 20:53:44 +0300

    Task 19

---
+++ src/secondhomework/Task19.java
@@ -0,0 +1,14 @@
+package secondhomework;
+
+public class Task19 {
+    public static void main(String[] args) {
+        int[] m = {7, 4, 6, 1, 0, 4, 7, 2, 3, 10};
+        for (int i = 0; i < m.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(m[i] + " ");
+            }
+        }
+    }
+}
+
+
