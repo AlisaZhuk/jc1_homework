de4e32fd9e76bc92bf0d335a22cc77561b360778
AlisaZhuk <Ahata01102011@gmail.com>
2022-03-13 22:59:33 +0300

    Task 20

--- src/secondhomework/Task20.java
+++ src/secondhomework/Task20.java
@@ -3,13 +3,19 @@ package secondhomework;
 public class Task20 {
     public static void main(String[] args) {
         int massive[] = {5, 36, 56, 2, 3, 87, 7, 57, 32, 3, 14};
-        int max = 0;
+        int maxValue = 0;
+        int maxIndex = 0;
+        System.out.print("Массив целочисленных чисел: ");
         for (int i = 0; i < massive.length; i++) {
-            if (massive[i] > max) {
-                max = massive[i];
+            System.out.print(+massive[i] + " ");
+            if (massive[i] > maxValue) {
+                maxValue = massive[i];
+                maxIndex = i;
             }
         }
-        System.out.println(max);
-    }
+        System.out.println();
+        System.out.println("Максимальное значение массива  " + maxValue);
+        System.out.println("Индекс максимального начения  " + maxIndex);
+            }
 }
 
