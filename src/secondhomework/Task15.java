4e9d45b88947828daec3ebdaae37ae35377cfb30
AlisaZhuk <Ahata01102011@gmail.com>
2022-03-13 23:32:34 +0300

    Task 15

--- src/secondhomework/Task15.java
+++ src/secondhomework/Task15.java
@@ -2,16 +2,21 @@ package secondhomework;
 
 public class Task15 {
     public static void main(String[] args) {
-        boolean b = true;
+        boolean second = false;
         for (int x = 50; x <= 70; x++) {
-            for (int i = 2; i < x; i++) {
+            boolean b = true;
+            for (int i = 2; i < 11; i++) {
                 if (x % i == 0) {
                     b = false;
-                    break;
                 }
             }
-            if (b) System.out.println(x);
-            else b = true;
+            if (b) {
+                if (second) {
+                    System.out.println(x);
+                    break;
+                }
+                second = true;
+            }
         }
     }
 }
