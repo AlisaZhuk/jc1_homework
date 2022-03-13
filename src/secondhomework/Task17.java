379b857c0f50c9d029a58c97d76ce77b2d321358
AlisaZhuk <Ahata01102011@gmail.com>
2022-03-13 23:40:42 +0300

    Task 17

---
+++ src/secondhomework/Task17.java
@@ -0,0 +1,11 @@
+package secondhomework;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Task17 {
+    public static void main(String[] args) {
+        long number = 20023143;
+        System.out.println(new DecimalFormat("###,###").format(number));
+    }
+}
+
