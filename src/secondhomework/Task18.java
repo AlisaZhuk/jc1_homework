6b305c55e051ce15f1a62f7ea21c2f00b88259ea
AlisaZhuk <Ahata01102011@gmail.com>
2022-03-29 11:33:09 +0300

    Task 6

--- src/secondhomework/Task18.java
+++ src/secondhomework/Task18.java
@@ -3,6 +3,7 @@ package secondhomework;
 public class Task18 {
     public static void main(String[] args) {
                int [] m = new int [10];
+        System.out.print("Массив целочисленных чисел: ");
         for(int i = 0; i < m.length;i ++) {
             m[i] = (int) (Math.random() * 10 - 1);
             System.out.print(m[i] + " ");
